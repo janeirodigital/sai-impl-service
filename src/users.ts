a7d68ae77a5ce03ffcc7810b947a69492582fba5
elf Pavlik <elf-pavlik@hackers4peace.net>
2022-03-02 12:23:32 -0600

    use @solid/access-token-verifier
    Co-authored-by: Maciej Samoraj <samurex@users.noreply.github.com>

--- src/users.ts
+++ src/users.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import jwtDecode from "jwt-decode";
+import { createSolidTokenVerifier, RequestMethod } from '@solid/access-token-verifier';
 import { INTEROP } from "@janeirodigital/interop-namespaces";
 import SessionStorage from "./session-storage";
 
@@ -11,26 +11,41 @@ usersRouter.get("/:webIdB64", async (req: Request, res: Response) => {
   console.log(webIdFromAgent)
   const sai = SessionStorage.get(webIdFromAgent);
 
-  // TODO rething how SessionStorage gets pupulated from redis - most likely on start
+  // TODO rethink how SessionStorage gets pupulated from redis - most likely on start
   if (!sai) {
     res.status(404).send();
     return;
   }
 
-  const tokenHeader = req.header("Authorization")?.split(" ")[1]; // Leave the `DPoP ` behind.
-
-  if (!tokenHeader) {
+  const verifier = createSolidTokenVerifier()
+  const { headers: { authorization, dpop }, method } = req;
+  if (!authorization || !dpop) {
     res.status(401).send();
     return;
   }
-
-  // TODO (angel) verify that the token is valid. jwtDecode does not perform signature verification
   // TODO solid-oidc : update to azp once changed to uma style as
-  const { webid: webIdFromToken, client_id: applicationId } = jwtDecode<{ webid: string, client_id: string }>(tokenHeader);
+  let webIdFromToken, applicationId
+  try {
+    const token = await verifier(
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      authorization!,
+      {
+        header: dpop as string,
+        method: method as RequestMethod,
+        url: `${process.env.SERVICE_URL}/users${req.url}`,
+      },
+    );
+    webIdFromToken = token.webid
+    applicationId = token.client_id
+
+  } catch (error: unknown) {
+    const message = `Error verifying WebID via DPoP-bound access token: ${(error as Error).message}`;
+    throw new Error(message);
+  }
 
   let registrationIri: string
   if (webIdFromAgent === webIdFromToken) {
-    // registration owner wants to discover applicatiaon registrqtaion
+    // registration owner wants to discover application registrataion
     // TODO sai-js: provide sai.findApplicationRegistration(clientId)
     for await (const registration of sai.applicationRegistrations) {
       if (registration.registeredAgent === applicationId) {
@@ -49,6 +64,7 @@ usersRouter.get("/:webIdB64", async (req: Request, res: Response) => {
     }
   }
 
+  // TODO: if (!registrationIri)
   res
     .status(200)
     .header("content-type", "text/turtle")
@@ -58,9 +74,6 @@ usersRouter.get("/:webIdB64", async (req: Request, res: Response) => {
       `<${applicationId}>; anchor="${registrationIri!}"; rel="${INTEROP.registeredAgent.value}"`
     )
     .send();
-
-
-  res.status(401).header("content-type", "text/turtle").send();
 });
 
 export default usersRouter;
