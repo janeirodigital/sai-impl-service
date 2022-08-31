2ef9da0343955748e30da162917c7745731fe95a
elf Pavlik <elf-pavlik@hackers4peace.net>
2022-08-31 13:19:33 -0500

    base64 enceded WebID in agentUrl
    Co-authored-by: Maciej Samoraj <samurex@users.noreply.github.com>

--- packages/mocks/src/mocked-session-manager.ts
+++ packages/mocks/src/mocked-session-manager.ts
@@ -5,9 +5,6 @@ import { ISessionManager } from "@janeirodigital/sai-server-interfaces"
 
 export class MockedSessionManager implements ISessionManager {
   constructor(public storage: IStorage) {}
-  getSaiSession = jest.fn(async(webId: string): Promise<AuthorizationAgent | undefined> => { return undefined })
+  getSaiSession = jest.fn(async(webId: string): Promise<AuthorizationAgent> => { return undefined as unknown as AuthorizationAgent })
   getOidcSession = jest.fn (async(webId: string): Promise<Session> => { return undefined as unknown as Session })
-  getFromAgentUrl = jest.fn (async(agentUrl: string): Promise<AuthorizationAgent | undefined> => { return undefined })
-  getWebId = jest.fn (async(agentUrl: string): Promise<string | undefined> => { return undefined })
-  getAgentUrlForSession = jest.fn(async (oidcSession: Session): Promise<string> => {return undefined as unknown as string})
 }
--- packages/service/src/session-manager.ts
+++ packages/service/src/session-manager.ts
@@ -2,20 +2,11 @@ import { randomUUID } from "crypto";
 import { getSessionFromStorage, IStorage, Session } from "@inrupt/solid-client-authn-node";
 import { AuthorizationAgent } from "@janeirodigital/interop-authorization-agent";
 import { ISessionManager } from "@janeirodigital/sai-server-interfaces"
-
-import { uuid2agentUrl } from "./url-templates";
+import { webId2agentUrl } from "./url-templates";
 
 type WebId = string;
 
 const cache = new Map<WebId, AuthorizationAgent>();
-
-export function getAgentUrlKey(clientId: string): string {
-  return `agentUrl:${clientId}`;
-}
-
-export function getWebIdKey(webId: string): string {
-  return `webId:${webId}`;
-}
 
 async function buildSaiSession(
   oidcSession: Session,
@@ -33,56 +24,24 @@ async function buildSaiSession(
 export class SessionManager implements ISessionManager {
   constructor(public storage: IStorage) {}
 
-  async getSaiSession(webId: string): Promise<AuthorizationAgent | undefined> {
+  async getSaiSession(webId: string): Promise<AuthorizationAgent> {
     const cached = cache.get(webId);
     if (cached) return cached;
+
     const oidcSession = await this.getOidcSession(webId)
-    if (oidcSession) {
-      const agentUrl = await this.getAgentUrlForSession(oidcSession)
-      const saiSession = await buildSaiSession(oidcSession, agentUrl);
-      cache.set(webId, saiSession);
-      return saiSession;
-    }
-  }
-
-  private async buildOidcSession(webId: string): Promise<Session> {
-      const agentUrl = uuid2agentUrl(randomUUID());
-
-      // set bi-directional mapping between webId and agentUrl
-      await this.storage.set(getAgentUrlKey(agentUrl), webId)
-      await this.storage.set(getWebIdKey(webId), agentUrl)
-
-      return new Session({ storage: this.storage }, webId);
-
+    const agentUrl = webId2agentUrl(webId)
+    const saiSession = await buildSaiSession(oidcSession, agentUrl);
+    cache.set(webId, saiSession);
+    return saiSession;
   }
 
   async getOidcSession(webId: string): Promise<Session> {
     let session = await getSessionFromStorage(webId, this.storage);
 
     if (!session) {
-      session = await this.buildOidcSession(webId)
+      session =  new Session({ storage: this.storage }, webId);
     }
 
     return session;
-  }
-
-  async getFromAgentUrl(agentUrl: string): Promise<AuthorizationAgent | undefined> {
-    const webId = await this.getWebId(agentUrl);
-    return webId ? this.getSaiSession(webId) : undefined;
-  }
-
-  async getWebId(agentUrl: string): Promise<string | undefined> {
-    return this.storage.get(getAgentUrlKey(agentUrl));
-  }
-
-  // assumes that oidcSession was created using buildOidcSession
-  // and mapping was set
-  async getAgentUrlForSession(oidcSession: Session): Promise<string> {
-    // we use WebID as sessionId and it is available even before logging in
-    const webId = oidcSession.info.sessionId
-    const agentUrl = await this.storage.get(getWebIdKey(webId));
-    // this should only happen if storage is corrupted
-    if (!agentUrl) throw new Error(`agentUrl not found for: ${webId}`)
-    return agentUrl
   }
 }
