674c891a6dc8278bc15d5ab0467ab28a3d14a0e7
elf Pavlik <elf-pavlik@hackers4peace.net>
2022-10-26 13:44:08 -0500

    [service] removed rdf helpers (not used any more)

--- packages/service/src/utils/rdf-parser.ts
+++
@@ -1,8 +0,0 @@
-import type { DatasetCore, Quad_Object, Quad_Subject } from "@rdfjs/types";
-
-export const getOneObject = (set: DatasetCore): Quad_Object | undefined => [...set].shift()?.object;
-export const getOneSubject = (set: DatasetCore): Quad_Subject | undefined =>
-  [...set].shift()?.subject;
-
-export const getAllObjects = (set: DatasetCore): Quad_Object[] => [...set].map((q) => q.object);
-export const getAllSubjects = (set: DatasetCore): Quad_Subject[] => [...set].map((q) => q.subject);
