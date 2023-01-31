05ab5d4e9122eed62f2604df60e0dff93f8f15b7
Angel Araya <angel.araya@janeirodigital.com>
2023-01-30 18:53:04 -0600

    Fix env vars

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-envsubst < /service/deploy/config-template.json > /service/packages/service/config/config.json
+envsubst < /service/deploy/config-template.json > /service/packages/service/config/config-prod.json
 
 cd /service/packages/service/ || exit
 
-npm start -- --config=config/config.json
+npm start -- --config=config/config-prod.json
 
