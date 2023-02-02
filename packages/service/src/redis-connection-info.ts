e598ffc88a58423d8dde6aaa9911ac8403869aad
Angel Araya <angel.araya@janeirodigital.com>
2023-02-02 17:30:53 -0600

    Update and fix config files

--- packages/service/src/redis-connection-info.ts
+++ packages/service/src/redis-connection-info.ts
@@ -2,8 +2,8 @@
 
 export class RedisConnectionInfo {
   constructor(
-    public host: string,
-    public port: number,
+    public host?: string,
+    public port?: number,
     public username?: string,
     public password?: string,
     public db?: number,
